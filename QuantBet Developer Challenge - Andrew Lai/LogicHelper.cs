14cdab1e31ec1f5cfd3a70bfb7caa2efaffb7774
andyjustandy <andylaipro@gmail.com>
2018-01-23 09:32:26 +0000

    Auto, Html, Divisor Algorithm

---
+++ QuantBet Developer Challenge - Andrew Lai/LogicHelper.cs
@@ -0,0 +1,207 @@
+﻿using mshtml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuantBet_Developer_Challenge
+{
+    /// <summary>
+    /// Collection of the logic used to complete the QuantBet Developer Challenge.
+    /// </summary>
+    class LogicHelper
+    {
+        bool debug = false;
+
+        /// <summary>
+        /// Performs the Divisor Algorithm on the two input numbers, then returns the answer.
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2"></param>
+        /// <returns> Returns the result of the Divisor Algorithm </returns>
+        public int Calculate_Divisor_Algorithm(long number1, long number2)
+        {
+            // Holds the highest common denominator between two numbers
+            int result = 0;
+            long dividend;
+            long divisor;
+            long quotent;
+            long remainder;
+
+            try
+            {
+                // Divisor Algorithm Begins: Identify the (Dividend), (Divisor), (Quotend), (Remainder)
+                if (number1 >= number2)
+                {
+                    dividend = number1;
+                    divisor = number2;
+                }
+                else
+                {
+                    dividend = number2;
+                    divisor = number1;
+                }
+                quotent = dividend / divisor;
+                remainder = dividend - (divisor * quotent);
+
+                // If the remainder is equal to zero, highest common denominator is found (same numbers)
+                if (remainder == 0)
+                {
+                    result = (int)divisor;
+                }
+                else
+                {
+                    // While the remainder is not equal to 0, keep performing the Divisor Algorithm
+                    while (remainder != 0)
+                    {
+                        dividend = divisor;
+                        divisor = remainder;
+                        quotent = dividend / divisor;
+                        remainder = dividend - (divisor * quotent);
+                    }
+                    // When the remainder is zero, the highest common denominator is equal to the divisor
+                    result = (int)divisor;
+                }
+                // Sets the result to the Results textbox on screen
+                return result;
+            }
+            catch
+            {
+                // If there are no number found a message to the UI is sent
+                if(debug) MessageBox.Show("Error: No numbers found at this URL");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 1) Extracts the 2 numbers from the HTML 
+        /// 2) calculates the solution using Division Algorithm
+        /// 3) Inputs the answer to the webpage
+        /// 4) Submits the answer by clicking the go button
+        /// </summary>
+        public void Extraction_Calculate_And_Submit(WebBrowser webbrowser_display)
+        {
+            // Extracts the numbers from the html and stores them into an array
+            long[] numbers = Extract_Numbers(webbrowser_display);
+
+            if (numbers.Length == 2)
+            {
+                // Calculates and stores the answer from the Divisor Algorithm
+                long answer = Calculate_Divisor_Algorithm(numbers[0], numbers[1]);
+
+                // Inputs the answer to the QuantBet webpage
+                Assign_Answer_To_Browser(webbrowser_display, answer);
+
+                // Locates and clicks the submit 'Go' button.
+                Simulate_Go_Click(webbrowser_display);
+            }
+        }
+
+        /// <summary>
+        /// Updates the display of the web browser display in the UI with the url input text. 
+        /// </summary>
+        public void Update_Webbrowser_Display(WebBrowser webbrowser_display, TextBox txt_URL)
+        {
+            try
+            {
+                // Checks to see if the input url is in the correct format
+                string url_string = txt_URL.Text.ToString();
+                if (!url_string.StartsWith("http://") && !url_string.StartsWith("https://"))
+                {
+                    url_string = "https://" + url_string;
+                }
+                // Navigates the the input url from the textbox
+                webbrowser_display.Navigate(url_string);
+            }
+            catch
+            {
+                // If the Url cannot be excecuted, nothing will happen.
+            }
+        }
+
+        /// <summary>
+        /// Extracts the numbers required to solved the challenge, then returns the numbers in an array (long).
+        /// </summary>
+        /// <returns> Returns the retreived numbers in an array of long integers. </returns>
+        private long[] Extract_Numbers(WebBrowser webbrowser_display)
+        {
+            // Initialises the list of cities and dates
+            List<long> numbers = new List<long>();
+            try
+            {
+                // Stores the html document from the currently loaded url
+                // Cast the web browser document as a mshtml.HTMLDocument
+                var document = webbrowser_display.Document as HTMLDocument;
+
+                // Gets all of the elements with the tag name: input 
+                var input_elements = document.getElementsByTagName("strong");
+
+                // Add the numbers into the numbers list
+                foreach (IHTMLElement element in input_elements)
+                {
+                    long number = Int64.Parse(element.innerText);
+                    numbers.Add(number);
+                }
+                // Return the numbers list in the form of an array
+                return numbers.ToArray();
+            }
+            catch (Exception e)
+            {
+                if (debug) MessageBox.Show("Could not extract numbers from the Url provided: " + e.ToString());
+                else MessageBox.Show("Could not extract numbers from the Url provided. ");
+            }
+            return numbers.ToArray();
+        }
+
+        /// <summary>
+        /// Assigns the answer number to the answer box field in the browser, using the HTML Document.
+        /// </summary>
+        private void Assign_Answer_To_Browser(WebBrowser webbrowser_display, long calculation_result)
+        {
+            // Required getting the reference: Microsoft.mshtml
+
+            // Cast the web browser document as a mshtml.HTMLDocument
+            var document = webbrowser_display.Document as HTMLDocument;
+
+            // Gets all of the elements with the tag name: input 
+            var input_elements = document.getElementsByTagName("input");
+
+            // Checks through each of the input elements, if there is a match assign the username/password to the input text
+            foreach (IHTMLElement element in input_elements)
+            {
+                // If the element's id is 'calchainput' the input box id, then input the calculation result
+                if (element.getAttribute("id") == "calchainput")
+                {
+                    element.innerText = calculation_result.ToString();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Locates the 'Go' submit button in the HTML and performs a click on the 'Go' button.
+        /// </summary>
+        private void Simulate_Go_Click(WebBrowser webbrowser_display)
+        {
+            // Required getting the reference: Microsoft.mshtml
+            // Cast the web browser document as a mshtml.HTMLDocument
+            var document = webbrowser_display.Document as HTMLDocument;
+
+            // Gets all of the elements with the tag name: input 
+            var input_buttons = document.getElementsByTagName("button");
+
+            // Checks through each of the button elements, if there is a match click the button
+            foreach (IHTMLElement button in input_buttons)
+            {
+                // If the element's class attribute is equal to 'btn btn--wide push-top--20' the Go button, click the button 
+                if (button.getAttribute("className") == "btn btn--wide push-top--20")
+                {
+                    button.click();
+                }
+            }
+        }
+
+    }
+}
---
+++ QuantBet Developer Challenge - Andrew Lai/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using mshtml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace QuantBet_Developer_Challenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LogicHelper logic = new LogicHelper();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region ---   MANUAL SOLUTION   ---
+        // When the Calculate button is clicked, the Divisor Algorithm is performed on the two numbers.      
+        private void btn_Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            long number1 = 0;
+            long number2 = 0;
+
+            try
+            {
+                // Checks if two numbers have been input, by trying to parse the text
+                number1 = long.Parse(txtb_Number1.Text);
+                number2 = long.Parse(txtb_Number2.Text);
+                
+                // Performs to Diviser Algorithm to get the 'result' answer
+                long result = logic.Calculate_Divisor_Algorithm(number1, number2);
+
+                // Displays the answer to the GUI
+                txtb_Result.Content = "Answer = " + result.ToString();
+            }
+            catch
+            {
+                // Cannot parse numbers: Do nothing...
+            }
+        }
+
+        // Upon clicking the textbox to input a new number, the existing content is erased.
+        private void txtb_Number1_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txtb_Number1.Text = "";
+        }
+
+        // Upon clicking the textbox to input a new number, the existing content is erased.
+        private void txtb_Number2_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            txtb_Number2.Text = "";
+        }
+
+        // Validates the key input to allow only numeric values
+        private void txtb_Number1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Numeric_Validation(sender, e);
+            Number_Length_Validation(txtb_Number1, e);
+        }
+
+        // Validates the key input to allow only numeric values
+        private void txtb_Number2_KeyDown(object sender, KeyEventArgs e)
+        {
+            Numeric_Validation(sender, e);
+            Number_Length_Validation(txtb_Number2, e);
+        }
+
+        // Key validation for numeric textbox
+        public void Numeric_Validation(object sender, KeyEventArgs e)
+        {
+            {
+                // Allows numeric values (no decimal)
+                bool isNumPadNumeric = (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9);
+                bool isNumeric = (e.Key >= Key.D0 && e.Key <= Key.D9);
+
+                // Will not allow a number held plus a modifier (no decimal)
+                if ((isNumeric || isNumPadNumeric) && Keyboard.Modifiers != ModifierKeys.None)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                // Allows keyboard controls
+                bool isControl = ((Keyboard.Modifiers != ModifierKeys.None && Keyboard.Modifiers != ModifierKeys.Shift)
+                    || e.Key == Key.Back || e.Key == Key.Delete || e.Key == Key.Insert
+                    || e.Key == Key.Down || e.Key == Key.Left || e.Key == Key.Right || e.Key == Key.Up
+                    || e.Key == Key.Tab
+                    || e.Key == Key.PageDown || e.Key == Key.PageUp
+                    || e.Key == Key.Enter || e.Key == Key.Return || e.Key == Key.Escape
+                    || e.Key == Key.Home || e.Key == Key.End);
+
+                // Discard keys that is not a numeric value or keyboard control
+                e.Handled = !isControl && !isNumeric && !isNumPadNumeric;
+            }
+        }
+
+        // Checks the character length of the number entered, does not allow more than 19characters
+        private void Number_Length_Validation(TextBox textbox, KeyEventArgs e)
+        {
+            int MAX_LENGTH = 19;
+            if (textbox.Text.Count() >= MAX_LENGTH)
+            {
+                e.Handled = true;
+            }
+        }
+        #endregion
+
+        #region ---   AUTOMATIC SOLUTION   ---
+        // Search button will use the input Url link and display the webpage in the application browser.
+        private void btn_SearchUrl_Click(object sender, RoutedEventArgs e)
+        {
+            logic.Update_Webbrowser_Display(webbrowser_display, txt_URL);
+        }
+
+        // Extracts the 2 numbers from the HTML, calculates the solution using the Divisor Algorithm and submits the form.
+        private void btn_Calculate_And_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            logic.Extraction_Calculate_And_Submit(webbrowser_display);
+        }
+
+        /// <summary>
+        /// The automatic solution to the developers challenge. Performs all of the action:
+        /// - Loads the webpage in the application browser, using url
+        /// - Automatically extract and store required data to solve the math question
+        /// - Calculates the answer to the question using the Divisor Algorithm
+        /// - Inputs and submits the question to the webpage
+        /// 
+        /// Note: The time delay may need increasing from 200 (o.3 seconds) depending on how fast the users computer can load the webpage.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btn_AutomaticSubmission_Click(object sender, RoutedEventArgs e)
+        {
+            // Updates the browser display with url input
+            logic.Update_Webbrowser_Display(webbrowser_display, txt_URL);
+
+            // Waits for the webpage to load
+            await Task.Delay(300);
+
+            // Extracts the 2 numbers from the HTML, calculates the solution and submits the form
+            logic.Extraction_Calculate_And_Submit(webbrowser_display);
+        }
+        #endregion
+
+    }
+}
